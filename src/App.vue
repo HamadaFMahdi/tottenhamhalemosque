2ae51118e7a4589406d30d291e0ab16b87eff027
Hamada Fadil Mahdi <hamadamahdi2014@gmail.com>
2021-05-30 17:57:17 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  Welcome to the Tottenham Hale Mosque website. It is still under construction.
+  For any help, please email 
+  <a href="mailto:tottenhamhalemosque@gmail.com">tottenhamhalemosque@gmail.com</a>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
